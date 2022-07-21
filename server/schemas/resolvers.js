0a701194287eaf70a8fda7e538a05e984b0aac52
Brandon <brandonperez@brandons-mbp.lan>
2022-07-21 13:22:21 -0400

    Implemented client-side GraphQL query for homepage data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,34 +1,34 @@
-const {User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-const {signToken} = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
-        thoughts: async(parent, {username}) => {
-            const params = username ? {username} : {};
-            return Thought.find(params).sort({ createdAt: -1})
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 })
         },
-        thought: async (parent, {_id}) => {
-            return Thought.findOne({_id});
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
         },
         //get all users
-        users: async() => {
+        users: async () => {
             return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
 
         //get a user by username
-        user: async(parents, {username}) => {
+        user: async (parents, { username }) => {
             return User.findOne({ username })
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts')
         },
 
-        me: async(parent, args, context) => {
+        me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({_id: context.user._id})
+                const userData = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
                     .populate('thoughts')
                     .populate('friends')
@@ -41,33 +41,35 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-            return user;
+            const token = signToken(user);
+
+            return {token, user};
         },
-        login: async (parent, {email, password }) => {
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
-            if(!user) {
+            if (!user) {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
             const correctPw = await user.isCorrectPassword(password);
 
-            if(!correctPw) {
+            if (!correctPw) {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
             const token = signToken(user);
-            return {token, user};
+            return { token, user };
 
         },
-        addThought: async(parent, args, context) => {
-            if(context.user){
-                const thought = await Thought.create({...args, username: context.user.username });
+        addThought: async (parent, args, context) => {
+            if (context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username });
 
                 await User.findByIdAndUpdate(
-                    {_id: context.user._id},
-                    {$push: {thoughts: thought._id }},
-                    {new: true}
+                    { _id: context.user._id },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
                 );
 
                 return thought;
@@ -75,12 +77,12 @@ const resolvers = {
 
             throw new AuthenticationError('You need to be logged in!');
         },
-        addReaction: async (parent, {thoughtId, reactionBody }, context) => {
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
             if (context.user) {
                 const updatedThought = await Thought.findOneAndUpdate(
-                    {_id: thoughtId},
-                    {$push : {reactions: {reactionBody, username: context.user.username}}},
-                    {new: true, runValidators: true}
+                    { _id: thoughtId },
+                    { $push: { reactions: { reactionBody, username: context.user.username } } },
+                    { new: true, runValidators: true }
                 );
 
                 return updatedThought;
@@ -90,17 +92,17 @@ const resolvers = {
         },
         addFriend: async (parent, { friendId }, context) => {
             if (context.user) {
-              const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { friends: friendId } },
-                { new: true }
-              ).populate('friends');
-          
-              return updatedUser;
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { friends: friendId } },
+                    { new: true }
+                ).populate('friends');
+
+                return updatedUser;
             }
-          
+
             throw new AuthenticationError('You need to be logged in!');
-          }
+        }
     }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+const path = require('path')
+
 //import authentication token function 
-const {authMiddleware} = require('./utils/auth')
+const { authMiddleware } = require('./utils/auth')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -22,13 +25,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//serve up static assets
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+}
+
+
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       // log where we can go to test our GQL API
