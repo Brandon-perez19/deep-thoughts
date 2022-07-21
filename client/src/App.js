efb80526bd6536cb9608e657b040d87a7af65696
Brandon <brandonperez@brandons-mbp.lan>
2022-07-21 15:45:39 -0400

    implemented graphql queries and navigation for other pages.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -16,13 +22,40 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route
+                path='/'
+                element = { <Home />}
+              />
+              <Route
+              path='/login'
+              element={<Login />}
+              />
+              <Route
+              path='/signup'
+              element={<Signup />}
+              />
+              <Route path='/profile'>
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route
+                path='/thought/:id'
+                element={<SingleThought />}
+              />
+              <Route 
+              path='*'
+              element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,14 +2,14 @@ import { gql } from '@apollo/client';
 
 
 export const QUERY_THOUGHTS = gql`
-query thoughts ($username: String) {
-    thoughts(username: $username){
+query thoughts($username: String) {
+    thoughts(username: $username) {
         _id
         thoughtText
         createdAt
         username
         reactionCount
-        reaction {
+        reactions {
             _id
             createdAt
             username
@@ -17,3 +17,42 @@ query thoughts ($username: String) {
         }
     }
 }`;
+
+export const QUERY_THOUGHT = gql`
+query thought($id: ID!) {
+    thought(_id: $id) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+    }
+}
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
